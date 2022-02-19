7583b3e555b4b5566ce6ed8da7e5a59ee10dd09f
Drxckzyz <71843072+Drxckzyz@users.noreply.github.com>
2022-02-18 20:53:43 -0400

    Work

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,5 @@ dotenv.config()
 
 if (!process.env.TOKEN) throw new Error(`Ummmm token needed....`)
 export const DISCORD_TOKEN = process.env.TOKEN
+export const TEST_HOOK_ID = process.env.HOOK_ID as string
+export const TEST_HOOK_TOKEN = "Lr4ZrUnxkaa0-3Gt5IIbxVsYTPRXda7H3F5UOSo8vIDWp-P_hkNopFyRTqb6DI-ZnIqn"
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { DISCORD_TOKEN } from "./config"
+import { DISCORD_TOKEN, TEST_HOOK_ID, TEST_HOOK_TOKEN } from "./config"
 import { Bot } from "#bots/index"
+import { executeWebhook } from "#lib/index"
 
 const client = new Bot({
     token: DISCORD_TOKEN,
 })
-//client.start()
-client.gateway?.spawn()
-console.log(client)
-
-setInterval(() => {
-    client.rest
-}, 1000 * 1000 * 1000)
-// First
+client.start()
+executeWebhook(client, TEST_HOOK_ID, "Lr4ZrUnxkaa0-3Gt5IIbxVsYTPRXda7H3F5UOSo8vIDWp-P_hkNopFyRTqb6DI-ZnIqn", {
+    content: "Test from Game Updates code",
+    username: "Game Updates",
+    embeds: [
+        {
+            description: "Hi"
+        }
+    ]
+})
